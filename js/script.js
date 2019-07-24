9c4c23a58ac02a64747195ea5ce31a683eb3d400
Magda Lejwoda <le.magda@o2.pl>
2019-07-24 19:28:26 +0200

    Add new js code with switch, case and return

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// let playerMove = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 const scissors = document.getElementById('scissors');
 const paper = document.getElementById('paper');
 const stone = document.getElementById('stone');
@@ -7,24 +5,23 @@ const stone = document.getElementById('stone');
 scissors.addEventListener('click', function () {
 	let userMove = 'scissors';
 	const computerMove = determineComputerMove();
-	result(userMove, computerMove);
+	const winner = result(userMove, computerMove);
+	console.log(winner);
 });
 
 paper.addEventListener('click', function () {
 	let userMove = 'paper';
 	const computerMove = determineComputerMove();
-	result(userMove, computerMove);
+	const winner = result(userMove, computerMove);
+	console.log(winner);
 });
 
 stone.addEventListener('click', function () {
 	let userMove = 'stone';
 	const computerMove = determineComputerMove();
-	result(userMove, computerMove);
+	const winner = result(userMove, computerMove);
+	console.log(winner);
 });
-
-function result(playerMove, computerMove) {
-	console.log(playerMove, computerMove);
-}
 
 function determineComputerMove() {
 	const randomComputerMove = Math.floor(Math.random() * 3 + 1);
@@ -35,33 +32,43 @@ function determineComputerMove() {
 			return 'paper';
 		case 3:
 			return 'stone';
+
 	}
 }
-function displayResult(computerMove, playerMove) {
-	console.log(computerMove, playerMove);
-if (computerMove == 'stone' && playerMove == 'paper') {
-	printMessage('You are winner!');
-} else if (computerMove == 'paper' && playerMove == 'paper') {
-	printMessage('The dead-heat. Try again');
-} else if (computerMove == 'scissors' && playerMove == 'paper') {
-	printMessage('You lost.');
-} else if (computerMove == 'stone' && playerMove == 'scissors') {
-	printMessage('You lost.');
-} else if (computerMove == 'stone' && playerMove == 'stone') {
-	printMessage('The dead-heat. Try again');
-} else if (computerMove == 'paper' && playerMove == 'scissors'){
-	printMessage('You are winner!');
-} else if (computerMove == 'paper' && playerMove == 'stone') {
-	printMessage('You lost.');
-} else if (computerMove == 'scissors' && playerMove == 'scissors') {
-	printMessage('The dead-heat. Try again');
-} else if (computerMove == 'scissors' && playerMove == 'stone') {
-	printMessage('You are winner!');
-} else { 
-	printMessage('Unknown move');
+
+function result(playerMove, computerMove) {
+	console.log(playerMove, computerMove);
+	switch (playerMove) {
+		case 'scissors':
+			if (computerMove === 'scissors') {
+				return 'draw';
+			}
+			if (computerMove === 'stone') {
+				return 'computer'
+			}
+			if (computerMove === 'paper') {
+				return 'user'
+			}
+			break;
+		case 'paper':
+			if (computerMove === 'scissors') {
+				return 'computer';
+			}
+			if (computerMove === 'stone') {
+				return 'user'
+			}
+			if (computerMove === 'paper') {
+				return 'draw'
+			}
+			break;
+		case 'stone':
+			switch (computerMove) {
+				case 'paper':
+					return 'computer';
+				case 'stone':
+					return 'draw';
+				case 'scissors':
+					return 'user';
+			}
+	}
 }
-
-return playerMove;
-}
-
-
