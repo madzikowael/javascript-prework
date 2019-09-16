bef2ea1799d854ec8962385b3b3f568b93c6f12b
Magda Lejwoda <le.magda@o2.pl>
2019-09-16 18:42:05 +0200

    Remove console logs and comments + change name function wino & lose

--- js/script.js
+++ js/script.js
@@ -60,103 +60,75 @@ const playCompleteGame = function(userScore, computerScore){
      return result.innerHTML = "Computer is winner" + userScore + ': ' + computerScore;
    }
    }
-   console.log('play rounds', playGame);
-  /* Do play Game for 1 or 2 or 3 rounds */
+   //console.log('play rounds', playGame);
 
-  //result.innerHTML = 'You choose ' + input.value + ' rounds of game. Let s your move.'; 
-  //if(input > 0 & input <= 3){
- // result = startGame();
- // } else {
- //   result.innerHTML = 'Your move is error. Try again and choose number of rounds beetwen 1-3.';
-//  }
-
- // console.log('Number of rounds', startGame);
-  
-//};
-/*function startGame(){
-  startGameButton.addEventListener('click', input);
-  if(oninput == 1 ^ oninput == 2 ^ oninput == 3){
-  return main();
-  } else {
-    result.innerHTML = 'Your chose numbers of rounds is wrong. Try again';
-  }
-  console.log('you re clicked start button', startGame);
-}
-*/
-
+playCompleteGame();
 
 /* create computer move*/
 function determineComputerMove() {
   const buttons = ['stone', 'paper', 'scissors'];
   const randomNumber = Math.floor(Math.random() * 3);
-  //console.log('It is: ' + randomNumber);
+
   return buttons[randomNumber];
 }
-//console.log(determineComputerMove());
+
 
 /* create chooses who win, lose, and when it;s a draw or unknown move*/
 
-function win(userMove, computerMove) {
+function userWin(userMove, computerMove) {
   userScore++;
   userScoreSpan.innerHTML = userScore;
   computerScoreSpan.innerHTML = computerScore;
-  //console.log(userMove);
-  //console.log(computerMove);
+  
   result.innerHTML = userMove + "  beats  " + computerMove + ". You win!";
-  //console.log('win');
+  
 }
 
-function lose(userMove, computerMove) {
+function computerWin(userMove, computerMove) {
   computerScore++;
   userScoreSpan.innerHTML = userScore;
   computerScoreSpan.innerHTML = computerScore;
-  //console.log(userMove);
-  //console.log(computerMove);
-  result.innerHTML = userMove + "  loses  " + computerMove + ". You lost!";
-  //console.log('lost');
   
+  result.innerHTML = userMove + "  loses  " + computerMove + ". You lost!";
+    
 }
 function draw(userMove, computerMove) {
-  //console.log(userMove);
-  //console.log(computerMove);
   result.innerHTML = userMove + "  equals  " + computerMove + ". It is a draw!";
-  //console.log('draw');
+  
 }
-function unknowUserMove(userMove, computerMove) { //add
-  result.innerHTML = "Wrong move. Try again. Good Luck."; //add
-  //console.log (unknowUserMove);
+function unknowUserMove(userMove, computerMove) { 
+  result.innerHTML = "Wrong move. Try again. Good Luck."; 
+  
 }
 
-/*create launch game*/
+/*create one launch game*/
 
 function playGame(userMove) {
 
-  //console.log('Look at ' + userMove)
   const computerMove = determineComputerMove();
-  //console.log("user move is.. " + userMove);
-  //console.log("comp move is.. " + computerMove);
+  
   switch (userMove + computerMove) {
     case "stonescissors":
     case "paperstone":
     case "scissorspaper":
-      win(userMove, computerMove);
-      //console.log("User wins.");
+      userWin(userMove, computerMove);
+      
       break;
     case "stonepaper":
     case "scissorsstone":
     case "paperscissors":
-      //console.log("User lost.");
-      lose(userMove, computerMove);
+      
+      computerWin(userMove, computerMove);
       break;
     case "stonestone":
     case "paperpaper":
     case "scissorsscissors":
       draw(userMove, computerMove);
-     //console.log("It's a draw.");
+     
       break;
       default:
         return unknowUserMove();
-        console.log('it is unknow move' + unknowMove);
+        
   }
 }
 
@@ -165,42 +137,31 @@ function playGame(userMove) {
 function main() {
   stone.addEventListener('click', function() {
     playGame('stone');
-    //console.log('You clicked stone.');
+    
   })
   paper.addEventListener('click', function() {
     playGame('paper');
-    //console.log('You clicked paper.');
+    
   })
   scissors.addEventListener('click', function(){
     playGame('scissors');
-    //console.log('You clicked scissors.');
+    
   })
-  fieldOfBody.addEventListener('click', function() { //add it
-    playGame('unknowMove'); //add it
-    //console.log('You clicked in body. Unknow move')
+  fieldOfBody.addEventListener('click', function() { 
+    playGame('unknowMove'); 
+    
   })
 }
 
 main();
 
-/*this function must to give me 0:0 on my scoreboard*/
+/* button reset give me null input.value*/
 
-/*resetGameButton = function resetGame(){
-  userScore.innerHTML = 0;
-  computerScore.innerHTML = 0;
-  console.log(resetGame, 'reset gry')
-}*/
-
-//function resetGame(){
- // resetGameButton.addEventListener('click', resetGame);
-//  result.innerHTML = input == 0;
- // console.log('reset Game', resetGame);//
-//};
 const form = document.getElementById('placeToWriteNumber');
-form.addEventListener('reset', function(e){
-  const input = document.getElementById('reset-game');
-  const value = input.value;
-  input.value = '';
+  form.addEventListener('reset', function(e){
+    const input = document.getElementById('reset-game');
+    const value = input.value;
+    input.value = '';
   
 });
 
